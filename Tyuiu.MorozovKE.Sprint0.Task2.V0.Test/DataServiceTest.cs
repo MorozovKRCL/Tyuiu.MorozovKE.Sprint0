0f746e0e0c752015dd3f81e7be7f4b74a8fbe798
MorozovKE <krekodill@gmail.com>
2025-10-01 03:38:11 +0500

    Task2

---
+++ Tyuiu.MorozovKE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MorozovKE.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MorozovKE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Кирилл";
+            var res = DataService.GetMessage(name);
+
+            //вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет, Кирилл", res);
+        }
+    }
+}
---
+++ Tyuiu.MorozovKe.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MorozovKE.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.MorozovKE.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // вызов класса DataSrvice и метода GetMessage
+            // из библиотеки
+            Console.WriteLine(DataService.GetMessage("Кирилл"));
+            Console.ReadKey();
+        }
+    }
+}
