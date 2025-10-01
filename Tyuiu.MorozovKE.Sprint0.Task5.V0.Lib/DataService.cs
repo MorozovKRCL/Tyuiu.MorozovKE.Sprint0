8f296dbac5ee0d1a340718c3c7f2172d32b0619a
MorozovKE <krekodill@gmail.com>
2025-10-01 06:31:15 +0500

    Task5

---
+++ Tyuiu.MorozovKE.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿namespace Tyuiu.MorozovKE.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        //пример линейной структуры
+
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                // пример для демонстрации ветвления
+                // в реальных проектах нужно использовать Exception
+                Console.WriteLine("Применение b = {0} на ноль делить нельзя", b);
+                return -1;
+            }
+            else 
+            {
+                return a / b;
+            }
+        }
+    }
+}
---
+++ Tyuiu.MorozovKE.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MorozovKE.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.MorozovKE.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("А + В = " + DataService.Addition(1, 5));
+            Console.WriteLine("А - В = " + DataService.Substraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataService.Division(5, 0));
+            Console.ReadKey();
+        }
+    }
+}
