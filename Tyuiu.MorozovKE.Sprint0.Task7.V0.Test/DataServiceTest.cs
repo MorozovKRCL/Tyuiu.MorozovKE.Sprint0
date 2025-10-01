b219bbca58c77986388dfc631838ecb7fbeff6e3
MorozovKE <krekodill@gmail.com>
2025-10-01 08:55:29 +0500

    Task7

---
+++ Tyuiu.MorozovKE.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.MorozovKE.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.MorozovKE.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditinArrayValid()
+        {
+            int[] nums1 = { 1, 1, 1, 1, 1 };
+            int[] nums2 = { 1, 1, 1, 1, 1 };
+            int[] nums3 = { 2, 2, 2, 2, 2 };
+
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEqual(nums3, res);
+
+
+        }
+    }
+}
