1feba8168d54954ea2d037ca1a2b0f811c01b446
MorozovKE <krekodill@gmail.com>
2025-10-01 05:08:49 +0500

    Task3

---
+++ Tyuiu.MorozovKE.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MorozovKE.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        // реализация метода Sum
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
