650dea1d0c36b266d03f84daa79b1c0c2af505eb
MorozovKE <krekodill@gmail.com>
2025-10-01 05:44:15 +0500

    Task4

---
+++ Tyuiu.MorozovKE.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿namespace Tyuiu.MorozovKE.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        //пример линейной структуры
+
+        public static int Addition(int a, int b)
+        {
+            return a + b; 
+        }
+
+        public static int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        
+        public static double Division(int a, int b)
+        {
+            return a / b; 
+        }
+    }
+}
---
+++ Tyuiu.MorozovKE.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MorozovKE.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.MorozovKE.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // пример линейной структуры программы
+            // вызов методов сложения, вычитания и тд
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Substraction(28, 11));
+            Console.WriteLine(DataService.Multiplication(27, 3));
+            Console.WriteLine(DataService.Division(18, 9));
+            Console.ReadLine();
+        }
+    }
+}
