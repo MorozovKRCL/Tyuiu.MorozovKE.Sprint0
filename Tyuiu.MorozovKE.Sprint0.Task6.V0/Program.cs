a76f5cf45131781f8300c544a0b94a7f016ba232
MorozovKE <krekodill@gmail.com>
2025-10-01 07:44:24 +0500

    Task6

---
+++ Tyuiu.MorozovKE.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.MorozovKE.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.MorozovKE.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            //AddictionArray
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+
+            //SubstractionArray
+            Console.WriteLine("разность элементов массива = " + DataService.SubstractionArray(numsArray));
+
+            //MultiplicationArray
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
